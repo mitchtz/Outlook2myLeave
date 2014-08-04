1dddbe176be3193c356844ce25f20d63917e625e
mitchtz <mitchtz@users.noreply.github.com>
2014-08-04 14:32:18 -0600

    Update Outlook2myLeave.py
    Add selenium testing, can open myCUinfo and navigate to myLeave

--- Outlook2myLeave.py
+++ Outlook2myLeave.py
@@ -1,6 +1,10 @@
 import win32com.client
 import time
 import datetime
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import getpass
 
 #myOlApp = CreateObject("Outlook.Application")
 #myItem = myOlApp.CreateItem(olAppointmentItem)
@@ -23,19 +27,47 @@ restrictedItems = appointments.Restrict(restriction)
 
 
 # Iterate through restricted AppointmentItems and print them
-for appointmentItem in restrictedItems:
-    print("Subject: {0}, Start: {1}, End: {2}".format(
-          appointmentItem.Subject, appointmentItem.Start, 
-          appointmentItem.End))
+#for appointmentItem in restrictedItems:
+
+#    #Prints the subject, start time, and end time in columns
+#    #print("Subject: {0}, Start: {1}, End: {2}".format(
+#    #      appointmentItem.Subject, appointmentItem.Start, 
+#    #      appointmentItem.End))
+#    if("Off" in appointmentItem.Subject):
+#        print("Subject: {0}, Start: {1}, End: {2}".format(
+#           appointmentItem.Subject, appointmentItem.Start, 
+#           appointmentItem.End))
+
+
+#Experiment w/ selenium
+
+browser = webdriver.Firefox() #Open browser
+print('Opening browser')
+browser.get('https://portal.prod.cu.edu/MyCUInfoFedAuthLogin.html') #Go to a web page
+assert 'myCUinfo' in browser.title #Assert that page has loaded
+elem = browser.find_element_by_id('username')  # Find the username
+elem.send_keys(input('username: ')) # + Keys.RETURN presses return
+elem = browser.find_element_by_id('password')  # Find the password
+elem.send_keys(getpass.getpass()) #Uses send keys to type password
+
+browser.find_element_by_id('submit').click()
+#elem.click()
+browser.get('https://portal.prod.cu.edu/psp/epprod/UCB2/ENTP/h/?tab=CU_STAFF')
+assert 'MyCUinfo' in browser.title #Assert that page has loaded
+browser.get('https://portal.prod.cu.edu/psp/epprod_newwin/EMPLOYEE/ENTP/s/WEBLIB_CU_S_MEN.ISCRIPT2.FieldFormula.IScript_leave?PORTALPARAM_PTCNAV=CU_LEAVE_TG&EOPP.SCNode=ENTP&EOPP.SCPortal=UCB2&EOPP.SCName=ADMN_CU_QUICK_LINKS&EOPP.SCLabel=CU%20Quick%20Links&EOPP.SCPTcname=&FolderPath=PORTAL_ROOT_OBJECT.PORTAL_BASE_DATA.CO_NAVIGATION_COLLECTIONS.ADMN_CU_QUICK_LINKS.ADMN_S201308091032101805630266&IsFolder=false')
 
 
 
 
-##print appointments.count 
-#x = 4 # This is a number for one of the calendar entries 
-#print(appointments[x])
-#print(appointments[x].start)
-#print(appointments[x].end)
-#print(appointments[x].RecurrenceState) 
-#print(appointments[x].Subject)
-#print(appointments[x].IsRecurring)
+
+
+
+
+
+
+
+
+
+
+
+
